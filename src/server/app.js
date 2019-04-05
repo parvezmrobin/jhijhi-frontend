8d74fbe48a7970130c57bdfea9c62976e49cd1be
parvezmahbub <parvez@nerddevs.com>
2019-04-05 14:05:47 +0600

    global error handlers

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { join } = require('path');
@@ -27,5 +28,30 @@ require('./authentication')(app);
 app.use('/', indexRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+// noinspection JSUnusedLocalSymbols
+app.use(function (err, req, res, next) {
+  // generate the error
+  res.status(err.status || 500);
+  const error = {};
+
+  Object.getOwnPropertyNames(err).forEach(function (key) {
+    error[key] = err[key];
+  });
+
+  error.stack = error.stack.split("\n").map(str => str.trim());
+
+  // only providing error in development
+  res.json({
+    message: error.message,
+    error: req.app.get('env') === 'development' ? error : {}
+  });
+});
 
 module.exports = app;
