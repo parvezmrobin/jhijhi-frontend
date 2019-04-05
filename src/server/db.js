2ebf992820e71ba166ffcfbc2ae30628a6a701c6
parvezmahbub <parvez@nerddevs.com>
2019-04-05 13:02:04 +0600

    updated `express-session` options

--- src/server/db.js
+++ src/server/db.js
@@ -19,6 +19,8 @@ module.exports = function (app) {
       app.use(session({
         secret: app.get('db'),
         store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        resave: false,
+        saveUninitialized: false,
       }));
       app.use(passport.session({}));
     });
