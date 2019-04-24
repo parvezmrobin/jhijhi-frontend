06d51c333c0c8f565a07fc9670318326c33dcdc8
parvezmrobin@gmail.com <qwer56uiop>
2019-04-24 17:00:53 +0600

    Running Page refactor

--- src/client/components/Running.js
+++ src/client/components/Running.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
-import CenterContent from './layouts/CenterContent';
-import SidebarList from './SidebarList';
+import React, { Component } from 'react';
 import CurrentOver from './CurrentOver';
 import PreviousOvers from './PreviousOvers';
 import ScoreInput from './ScoreInput';
-import {bindMethods, optional, toTitleCase} from '../lib/utils';
+import { bindMethods, toTitleCase } from '../lib/utils';
 import Score from './Score';
-import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import BatsmanSelectModal from './BatsmanSelectModal';
-
+import PreviousOverModal from './PreviousOverModal';
+import ScoreCard from './ScoreCard';
 
 export class Running extends Component {
   constructor(props) {
@@ -43,7 +41,7 @@ export class Running extends Component {
       }));
     },
     onBatsmenSelect(batsmenValues) {
-      const [errors, indices] = this.validateAndGetSelectedBatsmen(batsmenValues);
+      const [errors, indices] = this._validateAndGetSelectedBatsmen(batsmenValues);
       if (errors) {
         return errors;
       }
@@ -52,15 +50,18 @@ export class Running extends Component {
         ...prevState,
         ...indices,
       }));
-      return {batsman1: null, batsman2: null};
+      return {
+        batsman1: null,
+        batsman2: null,
+      };
     },
   };
 
 
-  validateAndGetSelectedBatsmen(batsmenValues) {
-    const {batsman1, batsman2} = batsmenValues;
+  _validateAndGetSelectedBatsmen(batsmenValues) {
+    const { batsman1, batsman2 } = batsmenValues;
 
-    const {innings, battingTeamPlayers} = this.getCurrentInningsDescription();
+    const { innings, battingTeamPlayers } = this._getCurrentInningsDescription();
     const indices = {};
     if (batsman1) {
       indices.batsman1 = battingTeamPlayers.reduce((i, player, playerI) => {
@@ -74,12 +75,10 @@ export class Running extends Component {
     }
 
     if (!batsman1 && (indices.batsman2 === this.state.batsman1)) {
-      return [{batsman2: 'Already Selected As Batsman1'}]
+      return [{ batsman2: 'Already Selected As Batsman1' }];
     }
 
     const errors = {};
-
-
     for (const over of innings.overs) {
       for (const bowl of over.bowls) {
         const batsman = battingTeamPlayers[bowl.playedBy]._id;
@@ -103,20 +102,23 @@ export class Running extends Component {
     return [null, indices];
   }
 
-  getCurrentInningsDescription() {
-    const innings = this.getCurrentInnings();
-    const battingTeamPlayers = this.getBattingTeamPlayers();
-    return {innings, battingTeamPlayers};
+  _getCurrentInningsDescription() {
+    const innings = this._getCurrentInnings();
+    const battingTeamPlayers = this._getBattingTeamPlayers();
+    return {
+      innings,
+      battingTeamPlayers,
+    };
   }
 
-  getCurrentInnings() {
-    const {state, innings1, innings2} = this.state.match;
+  _getCurrentInnings() {
+    const { state, innings1, innings2 } = this.state.match;
     const innings = (state === 'running') ? innings1 : innings2;
     return innings;
   }
 
-  getBattingTeamPlayers() {
-    const {state, team1BatFirst, team1Players, team2Players} = this.state.match;
+  _getBattingTeamPlayers() {
+    const { state, team1BatFirst, team1Players, team2Players } = this.state.match;
     const battingTeamPlayers = (state === 'running')
       ? (team1BatFirst ? team1Players : team2Players)
       : (team1BatFirst ? team2Players : team1Players);
@@ -124,8 +126,8 @@ export class Running extends Component {
   }
 
   componentDidMount() {
-    let {batsman1, batsman2} = this.state;
-    const innings = this.getCurrentInnings();
+    let { batsman1, batsman2 } = this.state;
+    const innings = this._getCurrentInnings();
     const outBatsmen = [];
 
     outer: for (let i = innings.overs.length - 1; i >= 0; i--) {
@@ -140,7 +142,7 @@ export class Running extends Component {
 
         const batsman = bowl.playedBy;
         if (outBatsmen.indexOf(batsman) !== -1) {
-          continue
+          continue;
         }
 
         if (batsman1 == null) {
@@ -161,36 +163,28 @@ export class Running extends Component {
 
 
   render() {
-    const {match, overModal, batsman1, batsman2} = this.state;
-    const {innings1: {overs}} = match;
+    const { match, overModal, batsman1, batsman2 } = this.state;
+    const { innings1: { overs } } = match;
     const lastOver = overs[overs.length - 1];
 
-    const {name, team1, team2, team1WonToss, team1BatFirst, team1Players, team2Players, innings1, innings2, state} = match;
+    const { name, team1, team2, team1WonToss, team1BatFirst, team1Players, team2Players, innings1, innings2, state } = match;
     const battingTeamName = (state === 'running')
       ? (team1BatFirst ? team1.name : team2.name)
       : (team1BatFirst ? team2.name : team1.name);
     const [innings, inningsNo] = (state === 'running') ? [innings1, 1] : [innings2, 2];
-    const tossOwnerChoice = team1WonToss ? (team1BatFirst ? 'bat' : 'bowl') : (team1BatFirst ? 'bowl' : 'bat');
+    const tossOwnerChoice = team1WonToss ? (team1BatFirst ? 'bat' : 'bowl')
+      : (team1BatFirst ? 'bowl' : 'bat');
 
     const [battingTeamPlayers, bowlingTeamPlayers] = (battingTeamName === team1.name)
       ? [team1Players, team2Players] : [team2Players, team1Players];
 
-    const sidebarPlayerMapper = this.genSidebarPlayerMapper(innings, battingTeamPlayers);
-
-    const sidebarPlayerList = battingTeamPlayers
-      .map(({_id, name}) => ({
-        _id,
-        name,
-      }));
 
     const onCreaseBatsmanName = toTitleCase(battingTeamPlayers[batsman1] && battingTeamPlayers[batsman1].name, ' ');
     const onBowlersEnd = toTitleCase(battingTeamPlayers[batsman2] && battingTeamPlayers[batsman2].name, ' ');
     return <div className="row">
       <aside className="col-md-3 d-none d-lg-block">
-        <CenterContent col="col">
-          <SidebarList title={battingTeamName} itemClass="text-white"
-                       itemMapper={sidebarPlayerMapper} list={sidebarPlayerList}/>
-        </CenterContent>
+        <ScoreCard innings={innings} battingTeamName={battingTeamName}
+                   battingTeamPlayers={battingTeamPlayers}/>
       </aside>
       <main className="col bg-success">
         <div className="row px-1">
@@ -215,62 +209,12 @@ export class Running extends Component {
           </div>
         </div>
       </main>
-      <Modal isOpen={this.state.overModal.open} toggle={this.closeOverModal}>
-        <ModalHeader toggle={this.closeOverModal} className="text-primary">
-          Bowled by&nbsp;
-          <span className="font-italic">
-            {toTitleCase(optional(bowlingTeamPlayers[overModal.over.bowledBy]).name)}
-          </span>
-          (Over {overModal.overNo})
-        </ModalHeader>
-        <ModalBody>
-          <CurrentOver balls={overModal.over.bowls} battingTeam={battingTeamPlayers}/>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={this.closeOverModal}>Close</Button>
-        </ModalFooter>
-      </Modal>
-      <BatsmanSelectModal isOpen={!this.state.batsman1 || !this.state.batsman2} batsman1={batsman1} batsman2={batsman2}
+      <PreviousOverModal overModal={overModal} toggle={this.closeOverModal}
+                         bowlingTeamPlayers={bowlingTeamPlayers}
+                         battingTeamPlayers={battingTeamPlayers}/>
+      <BatsmanSelectModal isOpen={!this.state.batsman1 || !this.state.batsman2} batsman1={batsman1}
+                          batsman2={batsman2}
                           options={battingTeamPlayers} onSelect={this.onBatsmenSelect}/>
     </div>;
-  }
-
-  genSidebarPlayerMapper(innings, battingTeamPlayers) {
-    const sidebarContent = {};
-    for (const over of innings.overs) {
-      for (const bowl of over.bowls) {
-        const batsmanName = battingTeamPlayers[bowl.playedBy].name;
-        if (!sidebarContent[batsmanName]) {
-          sidebarContent[batsmanName] = {
-            run: 0,
-            isOut: null,
-          };
-        }
-
-        if (bowl.singles) {
-          sidebarContent[batsmanName].run += bowl.singles;
-        } else if (bowl.boundary.run && bowl.boundary.kind === 'regular') {
-          sidebarContent[batsmanName].run += bowl.boundary.run;
-        }
-        if (bowl.isWicket) {
-
-          const outBatsmanName = bowl.isWicket.player ? battingTeamPlayers[bowl.isWicket.player].name : batsmanName;
-          sidebarContent[outBatsmanName].isOut = bowl.isWicket.kind;
-        }
-      }
-    }
-
-    const sidebarPlayerMapper = ({name}) => {
-      if (!sidebarContent[name]) {
-        return toTitleCase(name, ' ');
-      }
-
-      const isOut = sidebarContent[name].isOut;
-      const className = isOut ? 'text-secondary' : 'text-primary';
-      return <span className={className}>
-        {toTitleCase(name, ' ')} ({sidebarContent[name].run}) - {isOut ? toTitleCase(isOut, ' ') : 'Playing'}
-      </span>;
-    };
-    return sidebarPlayerMapper;
   }
 }
---
+++ src/client/components/ScoreCard.js
@@ -0,0 +1,59 @@
+import SidebarList from './SidebarList';
+import CenterContent from './layouts/CenterContent';
+import React from 'react';
+import { toTitleCase } from '../lib/utils';
+
+function _genSidebarPlayerMapper(innings, battingTeamPlayers) {
+  const sidebarContent = {};
+  for (const over of innings.overs) {
+    for (const bowl of over.bowls) {
+      const batsmanName = battingTeamPlayers[bowl.playedBy].name;
+      if (!sidebarContent[batsmanName]) {
+        sidebarContent[batsmanName] = {
+          run: 0,
+          isOut: null,
+        };
+      }
+
+      if (bowl.singles) {
+        sidebarContent[batsmanName].run += bowl.singles;
+      } else if (bowl.boundary.run && bowl.boundary.kind === 'regular') {
+        sidebarContent[batsmanName].run += bowl.boundary.run;
+      }
+      if (bowl.isWicket) {
+        const outBatsmanName = bowl.isWicket.player ? battingTeamPlayers[bowl.isWicket.player].name : batsmanName;
+        sidebarContent[outBatsmanName].isOut = bowl.isWicket.kind;
+      }
+    }
+  }
+
+  const _sidebarPlayerMapper = ({name}) => {
+    if (!sidebarContent[name]) {
+      return toTitleCase(name, ' ');
+    }
+
+    const isOut = sidebarContent[name].isOut;
+    const className = isOut ? 'text-secondary' : 'text-primary';
+    const status = isOut ? toTitleCase(isOut, ' ') : 'Playing';
+    return <span className={className}>
+        {toTitleCase(name, ' ')} ({sidebarContent[name].run}) - {status}
+      </span>;
+  };
+  return _sidebarPlayerMapper;
+}
+
+export default function ScoreCard(props) {
+  const {innings, battingTeamName, battingTeamPlayers} = props;
+  const sidebarPlayerMapper = _genSidebarPlayerMapper(innings, battingTeamPlayers);
+
+  const sidebarPlayerList = battingTeamPlayers
+    .map(({_id, name}) => ({
+      _id,
+      name,
+    }));
+
+  return <CenterContent col="col">
+    <SidebarList title={battingTeamName} itemClass="text-white"
+                 itemMapper={sidebarPlayerMapper} list={sidebarPlayerList}/>
+  </CenterContent>
+}
