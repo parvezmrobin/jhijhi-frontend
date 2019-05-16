1c273461771539847b776411765c9d3c2ee7ed43
Parvez M Robin <parvezmrobin@gmail.com>
2019-05-17 02:35:57 +0600

    added `postbuild` script

--- src/server/console/postbuild.js
+++ src/server/console/postbuild.js
@@ -4,12 +4,12 @@
  * Date: May 16, 2019
  */
 
-const { renameSync, readdirSync } = require('fs');
+const { copyFileSync } = require('fs');
 const { join } = require('path');
 
 const baseDir = join(__dirname, '..', '..', '..');
 
-console.log(readdirSync(baseDir));
-renameSync(join(baseDir, 'dist'), join(baseDir, 'public'));
+copyFileSync(join(baseDir, 'statics', 'manifest.json'), join(baseDir, 'public', 'manifest.json'));
+copyFileSync(join(baseDir, 'statics', 'favicon.svg'), join(baseDir, 'public', 'favicon.svg'));
 
 process.exit(0);
