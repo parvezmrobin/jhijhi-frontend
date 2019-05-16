c8b81e242e25b11e74fc7d8bd26bd4bd6f3fb478
Parvez M Robin <parvezmrobin@gmail.com>
2019-05-16 20:59:07 +0600

    `heroku-postbuild` log files

--- src/server/console/postbuild.js
+++ src/server/console/postbuild.js
@@ -4,10 +4,12 @@
  * Date: May 16, 2019
  */
 
-const { renameSync } = require('fs');
+const { renameSync, readdirSync } = require('fs');
 const { join } = require('path');
 
 const baseDir = join(__dirname, '..', '..', '..');
+
+console.log(readdirSync(baseDir));
 renameSync(join(baseDir, 'dist'), join(baseDir, 'public'));
 
 process.exit(0);
