c842b4490e8c0055326920055db0185000ac8e1c
Parvez M Robin <parvezmrobin@gmail.com>
2019-05-19 14:36:49 +0600

    minimum player in each team validation

--- src/server/routes/matches.js
+++ src/server/routes/matches.js
@@ -57,12 +57,22 @@ const matchBeginValidations = [
     .isArray(),
   check('team1Captain', 'No captain selected')
     .exists({ checkFalsy: true }),
+  check('team1Captain', 'Team 1 must have at least two players')
+    .custom((_, {req}) => {
+      const team1Players = req.body.team1Players;
+      return team1Players && team1Players.length > 1;
+    }),
   check('team1Captain', 'Team 1 captain should be a team 1 player')
     .custom((team1Captain, { req }) => {
       return req.body.team1Players && req.body.team1Players.indexOf(team1Captain) !== -1;
     }),
   check('team2Captain', 'No captain selected')
     .exists({ checkFalsy: true }),
+  check('team2Captain', 'Team 2 must have at least two players')
+    .custom((_, {req}) => {
+      const team2Players = req.body.team2Players;
+      return team2Players && team2Players.length > 1;
+    }),
   check('team2Captain', 'Team 2 captain should be a team 2 player')
     .custom((team2Captain, { req }) => {
       return req.body.team2Players && req.body.team2Players.indexOf(team2Captain) !== -1;
