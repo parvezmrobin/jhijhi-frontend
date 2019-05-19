95710a32b3d048de08049f28b1dd1c9fc850bed8
Parvez M Robin <parvezmrobin@gmail.com>
2019-05-19 12:39:02 +0600

    unique validation bugfix

--- src/server/routes/players.js
+++ src/server/routes/players.js
@@ -8,36 +8,50 @@ const express = require('express');
  * @property {Function} delete
  */
 const router = express.Router();
-const Player = require("../models/player");
-const responses = require("../responses");
+const Player = require('../models/player');
+const responses = require('../responses');
 const passport = require('passport');
-const authenticateJwt = passport.authenticate.bind(passport, 'jwt', {session: false});
-const {check, validationResult} = require('express-validator/check');
+const authenticateJwt = passport.authenticate.bind(passport, 'jwt', { session: false });
+const { check, validationResult } = require('express-validator/check');
 
 
 const playerCreateValidations = [
-  check('name').exists({checkFalsy: true})
-    .custom(name => {
+  check('name')
+    .exists({ checkFalsy: true })
+    .custom((name, { req }) => {
       return new Promise(function (resolve, reject) {
-        Player.findOne({name: name}).exec().then(player => {
-          if (player) {
-            reject("Player Name already taken.");
-          } else {
-            resolve();
-          }
-        }).catch(reject);
+        Player.findOne({
+          name: name,
+          creator: req.user._id,
+        })
+          .exec()
+          .then(player => {
+            if (player) {
+              reject('Player Name already taken.');
+            } else {
+              resolve();
+            }
+          })
+          .catch(reject);
       });
     }),
-  check('jerseyNo').isInt({min: 1})
-    .custom(jerseyNo => {
+  check('jerseyNo')
+    .isInt({ min: 1 })
+    .custom((jerseyNo, { req }) => {
       return new Promise(function (resolve, reject) {
-        Player.findOne({jerseyNo: jerseyNo}).exec().then(player => {
-          if (player) {
-            reject("This jersey is already taken.");
-          } else {
-            resolve();
-          }
-        }).catch(reject);
+        Player.findOne({
+          jerseyNo: jerseyNo,
+          creator: req.user._id,
+        })
+          .exec()
+          .then(player => {
+            if (player) {
+              reject('This jersey is already taken.');
+            } else {
+              resolve();
+            }
+          })
+          .catch(reject);
       });
     }),
 ];
@@ -45,7 +59,7 @@ const playerCreateValidations = [
 /* GET players listing. */
 router.get('/', authenticateJwt(), (request, response) => {
   Player
-    .find({creator: request.user._id})
+    .find({ creator: request.user._id })
     .lean()
     .then(players => response.json(players))
     .catch(err => {
@@ -55,21 +69,28 @@ router.get('/', authenticateJwt(), (request, response) => {
         message: responses.players.index.err,
         err: err.error || err.errors || err,
       });
-    })
+    });
 });
 
-router.post('/', authenticateJwt(), playerCreateValidations, (request, response) =>{
+router.post('/', authenticateJwt(), playerCreateValidations, (request, response) => {
   const errors = validationResult(request);
-  const promise = errors.isEmpty() ? Promise.resolve() : Promise.reject({status: 400, errors: errors.array()});
-  const {name, jerseyNo} = request.body;
+  const promise = errors.isEmpty() ? Promise.resolve() : Promise.reject({
+    status: 400,
+    errors: errors.array(),
+  });
+  const { name, jerseyNo } = request.body;
 
   promise
-    .then(() => Player.create({name, jerseyNo, creator: request.user._id}))
+    .then(() => Player.create({
+      name,
+      jerseyNo,
+      creator: request.user._id,
+    }))
     .then(createdPlayer => {
       response.json({
         success: true,
         message: responses.players.create.ok(name),
-        player: {_id: createdPlayer._id},
+        player: { _id: createdPlayer._id },
       });
     })
     .catch(err => {
@@ -79,7 +100,7 @@ router.post('/', authenticateJwt(), playerCreateValidations, (request, response)
         message: responses.players.create.err,
         err: err.error || err.errors || err,
       });
-    })
+    });
 });
 
 module.exports = router;
