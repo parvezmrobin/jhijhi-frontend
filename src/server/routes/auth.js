0109d01d6d1b1ef82023a9d8d2b8f51e139dc82e
Parvez M Robin <parvezmrobin@gmail.com>
2019-05-23 18:14:34 +0600

    auth refactor

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -10,31 +10,39 @@ const express = require('express');
  */
 const router = express.Router();
 const passport = require('passport');
-const authenticateJwt = passport.authenticate.bind(passport, 'jwt', {session: false});
+const authenticateJwt = passport.authenticate.bind(passport, 'jwt', { session: false });
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const bcrypt = require("bcrypt");
-const responses = require("../responses");
-const {check, validationResult} = require('express-validator/check');
+const bcrypt = require('bcrypt');
+const responses = require('../responses');
+const { check, validationResult } = require('express-validator/check');
+const { sendErrorResponse } = require('../lib/utils');
 
 const registrationValidations = [
-  check('username').exists({checkFalsy: true})
+  check('username')
+    .exists({ checkFalsy: true }),
+  check('username')
     .custom(username => {
       return new Promise(function (resolve, reject) {
-        User.findOne({username: username}).exec().then(user => {
-          if (user) {
-            reject("Username already taken.");
-          } else {
-            resolve();
-          }
-        }).catch(reject);
+        User.findOne({ username: username })
+          .exec()
+          .then(user => {
+            if (user) {
+              reject('Username already taken.');
+            } else {
+              resolve();
+            }
+          })
+          .catch(reject);
       });
     }),
-  check('password').isLength({min: 4})
-    .custom((password, {req}) => {
+  check('password', 'Password should be at least 4 characters long')
+    .isLength({ min: 4 }),
+  check('password')
+    .custom((password, { req }) => {
       if (password !== req.body.confirm) {
         // trow error if passwords do not match
-        throw new Error("Password and confirm password don't match");
+        throw new Error('Password and confirm password don\'t match');
       }
       return password;
     }),
@@ -42,13 +50,16 @@ const registrationValidations = [
 
 
 router.get('/user', authenticateJwt(), function (request, response) {
-  response.json({username: request.user.username});
+  response.json({ username: request.user.username });
 });
 
 router.post('/register', registrationValidations, function (request, response) {
   const errors = validationResult(request);
-  const promise = errors.isEmpty() ? Promise.resolve() : Promise.reject({status: 400, errors: errors.array()});
-  const {username, password} = request.body;
+  const promise = errors.isEmpty() ? Promise.resolve() : Promise.reject({
+    status: 400,
+    errors: errors.array(),
+  });
+  const { username, password } = request.body;
   const saltRounds = 10;
 
   promise
@@ -63,38 +74,38 @@ router.post('/register', registrationValidations, function (request, response) {
       response.json({
         success: true,
         message: responses.auth.register.ok,
-        user: {_id: user._id},
+        user: { _id: user._id },
       });
     })
-    .catch(err => {
-      response.status(err.statusCode || err.status || 500);
-      response.json({
-        success: false,
-        message: responses.auth.register.err,
-        err: err.error || err.errors || err,
-      });
-    });
+    .catch(err => sendErrorResponse(response, err, responses.auth.register.err));
 });
 
 router.post('/login', function (request, response) {
-  const {username, password} = request.body;
+  const { username, password } = request.body;
 
   User
-    .findOne({username})
+    .findOne({ username })
     .exec()
     .then(user => {
       if (!user) {
-        return response.json({success: false});
+        return response.json({ success: false });
       }
       return bcrypt
         .compare(password, user.password)
         .then(matched => {
           if (matched) {
             const token = jwt.sign(user._id.toString(), request.app.get('db'));
-            return response.json({success: true, 'token': token});
+            return response.json({
+              success: true,
+              'token': token,
+            });
           }
-          return response.json({success: false});
+          return response.json({ success: false });
         });
+    })
+    .catch(err => {
+      console.error(err);
+      response.json({ success: false });
     });
 });
 
