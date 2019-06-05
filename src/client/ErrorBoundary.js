ef5580e66858867cf3bf300b59ae3c88845e8cc5
Parvez M Robin <parvezmrobin@gmail.com>
2019-06-06 02:26:24 +0600

    ErrorBoundary style update

--- src/client/ErrorBoundary.js
+++ src/client/ErrorBoundary.js
@@ -5,20 +5,20 @@
  */
 
 
-import React, {Component} from "react";
-import CenterContent from "./components/layouts/CenterContent";
+import React, { Component } from 'react';
+import CenterContent from './components/layouts/CenterContent';
 import { Link } from 'react-router-dom';
 
 
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = {hasError: false};
+    this.state = { hasError: false };
   }
 
   static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
-    return {hasError: true};
+    return { hasError: true };
   }
 
   componentDidCatch(error, info) {
@@ -28,15 +28,20 @@ class ErrorBoundary extends Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <CenterContent><h1 className="text-center font-weight-normal">
-        <span className="d-inline-flex text-danger">Something went wrong!</span>
-        <br/>
-        <span className="d-inline-flex mt-4">
+      return <CenterContent>
+        <h1 className="text-center font-weight-normal">
+          <span className="d-inline-flex text-danger mt-5 mt-md-0">Something went wrong!</span>
+          <br/>
+          <span className="d-inline-flex mt-4">
+          <span>
           Please&nbsp;
-          <Link to="#" className="text-decoration-none" onClick={window.location.reload}>reload</Link>
-          &nbsp;the page to retry.
+            <Link to="#" className="text-decoration-none"
+                  onClick={window.location.reload}>reload</Link>
+            &nbsp;the page to retry.
+          </span>
         </span>
-      </h1></CenterContent>;
+        </h1>
+      </CenterContent>;
     }
 
     return this.props.children;
