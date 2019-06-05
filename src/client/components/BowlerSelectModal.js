c8b73317d7763d5b63642b1ce0ee4587b0a409a7
Parvez M Robin <parvezmrobin@gmail.com>
2019-06-06 02:35:00 +0600

    typo fix

--- src/client/components/BowlerSelectModal.js
+++ src/client/components/BowlerSelectModal.js
@@ -10,7 +10,7 @@ export default class BowlerSelectModal extends Component {
     super(props);
     this.state = {
       bowler: '',
-      error: 'aaa',
+      error: '',
     };
     bindMethods(this);
   }
