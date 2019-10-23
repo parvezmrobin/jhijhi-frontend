f773d00e6e1977c99c16952d70c80229f7c26a3a
Parvez M Robin <parvezmrobin@gmail.com>
2019-10-23 23:44:38 +0600

    kidding password style fix

--- src/client/pages/Kidding.js
+++ src/client/pages/Kidding.js
@@ -6,7 +6,7 @@ export default function Kidding() {
   return (
     <CenterContent><h2 className="text-center mt-10 mt-lg-0">
       <span className="d-inline-block">Managing account with only <code>username</code> and <code>password</code>?</span>
-      &nbsp;
+      <span className="d-none d-md-inline">&nbsp;</span>
       <span className="d-inline-block">Are you kidding yourself?</span>
     </h2></CenterContent>
   );
--- src/client/pages/Password.js
+++ src/client/pages/Password.js
@@ -6,9 +6,9 @@ export default function Kidding() {
   return (
     <CenterContent><h2 className="text-center mt-10 mt-lg-0">
       <span className="d-inline-block">মিয়া, নিজেরে কী মনে হয়?</span>
-      &nbsp;
+      <span className="d-none d-sm-inline">&nbsp;</span>
       <span className="d-inline-block">আপনের অ্যাকাউন্ট কে হ্যাক করবে?</span>
-      &nbsp;
+      <span className="d-none d-md-inline">&nbsp;</span>
       <span className="d-inline-block" role="img" aria-label="frog">🐸🐸🐸</span>
     </h2></CenterContent>
   );
