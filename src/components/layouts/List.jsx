e6e1297a0ae4402f7f6c6ac3cb63c41019c6695f
Parvez M Robin <parvezmrobin@gmail.com>
2020-02-29 20:42:34 +0600

    collapsible sidebar list

--- src/components/layouts/List.jsx
+++ src/components/layouts/List.jsx
@@ -5,26 +5,47 @@
  */
 
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import InputControl from '../form/control/input';
+import { Collapse, Button } from "reactstrap";
 
 
-function List(props) {
-  const className = 'list-group-item bg-transparent ' + (props.itemClass || '');
-  const mapper = props.itemMapper || (item => item);
-  const items = props.list.map(
-    (item, i) => <li key={item._id} className={className}>{mapper(item, i)}</li>,
-  );
-  return (
-    <Fragment>
-      <h3>{props.title}</h3>
-      {props.onFilter && <InputControl autoFocus placeholder="Type here to filter list"
-                                       onChange={e => props.onFilter(e.target.value)}/>}
-      <hr/>
-      <ul className="list-group">{items}</ul>
-    </Fragment>
-  );
+class List extends React.Component {
+  state = {
+    isOpen: window.innerWidth > Number.parseInt(
+      getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-sm')
+    ), // in mobile view keep the list collapsed by default
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
+
+  render() {
+    const className = 'list-group-item bg-transparent ' + (this.props.itemClass || '');
+    const mapper = this.props.itemMapper || (item => item);
+    const items = this.props.list.map(
+      (item, i) => <li key={item._id} className={className}>{mapper(item, i)}</li>,
+    );
+    return (
+      <>
+        <h3>
+          {this.props.title}
+          <Button onClick={this.toggle} color="primary" className="float-right d-sm-none">
+            {this.state.isOpen ? 'Collapse' : 'Expand'}
+          </Button>
+        </h3>
+        <Collapse isOpen={this.state.isOpen}>
+          {this.props.onFilter
+          && <InputControl autoFocus placeholder="Type here to filter list"
+                           onChange={e => this.props.onFilter(e.target.value)}/>}
+          <hr/>
+          <ul className="list-group">{items}</ul>
+        </Collapse>
+      </>
+    );
+  }
 }
 
 List.propTypes = {
