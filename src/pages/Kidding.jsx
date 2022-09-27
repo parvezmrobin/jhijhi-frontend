3af593fb00fb2e49f56bfda30036c34597c7cff2
Parvez M Robin <parvezmrobin@gmail.com>
2022-09-27 04:53:58 -0300

    Fixed eslint errors in Kidding

--- src/pages/Kidding.jsx
+++ src/pages/Kidding.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import CenterContent from '../components/layouts/CenterContent';
 
-
 export default function Kidding() {
   return (
-    <CenterContent><h2 className="text-center mt-10 mt-lg-0">
-      <span className="d-inline-block">Managing account with only <code>username</code> and <code>password</code>?</span>
-      <span className="d-none d-md-inline">&nbsp;</span>
-      <span className="d-inline-block">Are you kidding yourself?</span>
-    </h2></CenterContent>
+    <CenterContent>
+      <h2 className="text-center mt-10 mt-lg-0">
+        <span className="d-inline-block">
+          Managing account with only <code>username</code> and{' '}
+          <code>password</code>?
+        </span>
+        <span className="d-none d-md-inline">&nbsp;</span>
+        <span className="d-inline-block">Are you kidding yourself?</span>
+      </h2>
+    </CenterContent>
   );
 }
