9be89f24cde1eafb9bce25f9277b76063f992b22
Parvez M Robin <parvezmrobin@gmail.com>
2022-09-27 02:25:42 -0300

    Fixed eslint errors in Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,9 +4,7 @@
  * Date: Apr 01, 2019
  */
 
-
 import React, { Component } from 'react';
-
 
 class Contact extends Component {
   componentDidMount() {
@@ -23,20 +21,21 @@ class Contact extends Component {
         <div className="col-md-8 offset-md-2 bg-dark-trans rounded">
           <div className="d-flex justify-content-center text-center v-100">
             <div className="col-auto text-white p-1 fs-1 fs-md-2">
-                <span>
-                  <span className="d-inline-block">Mail me at</span>
-                  {' '}
-                  <a className="d-inline-block" href="mailto:parvezmrobin@gmail.com">
-                    parvezmrobin@gmail.com
-                  </a>
-                </span>
+              <span>
+                <span className="d-inline-block">Mail me at</span>{' '}
+                <a
+                  className="d-inline-block"
+                  href="mailto:parvezmrobin@gmail.com"
+                >
+                  parvezmrobin@gmail.com
+                </a>
+              </span>
             </div>
           </div>
         </div>
       </div>
     );
   }
-
 }
 
 export default Contact;
