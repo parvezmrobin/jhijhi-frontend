52b46292bf05571b8a563ab3fdfbd1e1cf30a068
Parvez M Robin <parvezmrobin@gmail.com>
2022-10-28 06:30:04 -0300

    dark and clean BowlerSelectModal.jsx

--- src/components/modal/BowlerSelectModal.jsx
+++ src/components/modal/BowlerSelectModal.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
-import FormGroup from '../form/FormGroup';
 import * as PropTypes from 'prop-types';
+import { shape } from 'prop-types';
+import FormGroup from '../form/FormGroup';
 import { bindMethods, subtract } from '../../lib/utils';
 import fetcher from '../../lib/fetcher';
+import { PlayerType } from '../../types';
 
 export default class BowlerSelectModal extends Component {
+  handlers = {
+    onSelect() {
+      const { matchId, bowlers, onSelect } = this.props;
+      const { bowler } = this.state;
+      const bowlerIndex = bowlers.findIndex((b) => b._id === bowler);
+      if (bowlerIndex === -1) {
+        return this.setState({ error: 'Error while selecting the bowler' });
+      }
+      const data = { bowledBy: bowlerIndex };
+      return fetcher
+        .post(`matches/${matchId}/over`, data)
+        .then(() => onSelect(bowlerIndex));
+    },
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -15,60 +32,62 @@ export default class BowlerSelectModal extends Component {
     bindMethods(this);
   }
 
-  handlers = {
-    onSelect() {
-      const bowlerIndex = this.props.bowlers.findIndex(b => b._id === this.state.bowler);
-      if (bowlerIndex === -1) {
-        return this.setState({ error: 'Error while selecting the bowler' });
-      }
-      const data = { bowledBy: bowlerIndex };
-      return fetcher
-        .post(`matches/${this.props.matchId}/over`, data)
-        .then(() => this.props.onSelect(bowlerIndex));
-    },
-  };
-
-
+  // eslint-disable-next-line react/no-deprecated
   componentWillReceiveProps(nextProps) {
     const { bowlers, lastBowler } = nextProps;
-    const availableBowlers = (!lastBowler) ? bowlers
+    const availableBowlers = !lastBowler
+      ? bowlers
       : subtract(bowlers, [lastBowler], (b1, b2) => b1._id === b2._id);
     this.setState({ bowler: availableBowlers[0]._id });
   }
-
 
   componentWillUnmount() {
     fetcher.cancelAll();
   }
 
-
   render() {
     const { open, bowlers, lastBowler } = this.props;
+    const { bowler, error } = this.state;
 
-    const availableBowlers = (!lastBowler) ? bowlers
-      : subtract(bowlers, [lastBowler], (b1, b2) => b1._id === b2._id);
+    let availableBowlers;
+    if (!lastBowler) {
+      availableBowlers = bowlers;
+    } else {
+      availableBowlers = subtract(
+        bowlers,
+        [lastBowler],
+        (b1, b2) => b1._id === b2._id
+      );
+    }
 
-    return <Modal isOpen={open}>
-      <ModalHeader className="text-primary">
-        Select Bowler
-      </ModalHeader>
-      <ModalBody>
-        <FormGroup type="select" name="bowler" value={this.state.bowler}
-                   onChange={e => this.setState({ bowler: e.target.value })}
-                   options={availableBowlers} isValid={this.state.error ? false : null}
-                   feedback={this.state.error}/>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={this.onSelect}>Select</Button>
-      </ModalFooter>
-    </Modal>;
+    return (
+      <Modal isOpen={open} contentClassName="bg-dark">
+        <ModalHeader className="text-primary">Select Bowler</ModalHeader>
+        <ModalBody>
+          <FormGroup
+            type="select"
+            name="bowler"
+            value={bowler}
+            onChange={(e) => this.setState({ bowler: e.target.value })}
+            options={availableBowlers}
+            isValid={error ? false : null}
+            feedback={error}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.onSelect}>
+            Select
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
   }
 }
 
 BowlerSelectModal.propTypes = {
   open: PropTypes.bool,
   onSelect: PropTypes.func,
-  bowlers: PropTypes.array,
-  lastBowler: PropTypes.object,
+  bowlers: PropTypes.arrayOf(shape(PlayerType)),
+  lastBowler: shape(PlayerType),
   matchId: PropTypes.string,
 };
