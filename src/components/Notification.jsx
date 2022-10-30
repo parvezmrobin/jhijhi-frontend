7519ebb25c32d90b1f1ff52fbd33afbe8ceb42f1
Parvez M Robin <parvezmrobin@gmail.com>
2022-10-30 02:44:07 -0300

    Update Notification UI

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -5,24 +5,23 @@
  */
 
 import React from 'react';
-import { Toast, ToastBody, ToastHeader } from "reactstrap";
-import * as PropTypes from "prop-types";
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+import * as PropTypes from 'prop-types';
 
-
-export default function Notification(props) {
-  return <div className="sticky-top">
-    <Toast isOpen={!!props.message}>
-      <ToastHeader icon="primary" toggle={props.toggle}>
-        Jhijhi
-      </ToastHeader>
-      <ToastBody>
-        {props.message}
-      </ToastBody>
-    </Toast>
-  </div>;
+export default function Notification({ message, toggle }) {
+  return (
+    <div className="sticky-top">
+      <Toast isOpen={!!message}>
+        <ToastHeader icon="primary" toggle={toggle} className="text-primary">
+          Jhijhi
+        </ToastHeader>
+        <ToastBody className="bg-dark">{message}</ToastBody>
+      </Toast>
+    </div>
+  );
 }
 
 Notification.propTypes = {
-  message: PropTypes.any,
+  message: PropTypes.string,
   toggle: PropTypes.func,
 };
